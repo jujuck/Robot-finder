9dc9039d6640180650c05997726a3021e6fdef9c
Julien ichard WCS <julien.y.richard@gmail.com>
2022-03-31 15:37:54 +0200

    Initialisation du projet WorkShop RRobot finder, props, state and filtering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+/** Import de la donnée */
 import './App.css';
 
+import Robot from './data/data';
+
 function App() {
+  console.log(Robot)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Robot Finder</h1>
     </div>
   );
 }
