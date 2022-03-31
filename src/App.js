0f1df21cf1017aa2e686e3ea60a5ed45424e5ac2
Julien ichard WCS <julien.y.richard@gmail.com>
2022-03-31 15:51:03 +0200

    Step 1 : ajout du map et du début de la carte des robots

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 /** Import de la donnée */
 import './App.css';
 
-import Robot from './data/data';
+import Robots from './data/data';
 
 function App() {
-  console.log(Robot)
+  console.log(Robots)
   return (
     <div className="App">
       <h1>Robot Finder</h1>
+      {Robots.map((robot) => {
+        return (<div key={robot.id}>
+          <h3>{robot.name}</h3>
+        </div>)
+      })}
     </div>
   );
 }
